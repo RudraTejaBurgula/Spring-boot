4e96b08c37e4ddf6f5d5cb3d61de10edd083d4e0
Rudra Teja Burgula <101740@suneratech.com>
2022-07-24 13:09:34 +0530

    added

--- spring-cloud-config-server/src/main/java/com/micro/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/micro/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.micro.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
-
+//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
+	//http://localhost:8888/limits-service/default
 }
